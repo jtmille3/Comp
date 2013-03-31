45da3bbabbc07770871ff99428f0b4ea080b9bf3
Jeff Miller <jeffmiller@jeffmiller.name>
2013-03-30 21:28:30 -0400

    adding generated web content

---
+++ test/com/sas/comp/models/GoalTest.java
@@ -0,0 +1,17 @@
+package com.sas.comp.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sas.comp.hibernate.Hibernate;
+
+public class GoalTest {
+
+  @Test
+  public void testScorer() {
+    final Goal goal = Hibernate.getInstance().find(Goal.class, 1);
+    Assert.assertNotNull(goal.getId());
+    Assert.assertNotNull(goal.getGame());
+    Assert.assertNotNull(goal.getPlayer());
+  }
+}
