5f62e6b5c353c9486604f25625c647398ecc5789
Jeff Miller <jeffmiller@jeffmiller.name>
2013-03-30 21:34:53 -0400

    changed to tabs instead of spaces

--- src/com/sas/comp/listener/HibernateListener.java
+++ src/com/sas/comp/listener/HibernateListener.java
@@ -9,13 +9,13 @@ import com.sas.comp.hibernate.Hibernate;
 @WebListener
 public class HibernateListener implements ServletContextListener {
 
-  @Override
-  public void contextDestroyed(final ServletContextEvent arg0) {
-  }
+	@Override
+	public void contextDestroyed(final ServletContextEvent arg0) {
+	}
 
-  @Override
-  public void contextInitialized(final ServletContextEvent arg0) {
-    Hibernate.getInstance(); // initialize on server startup.  No lag on first page load.
-  }
+	@Override
+	public void contextInitialized(final ServletContextEvent arg0) {
+		Hibernate.getInstance(); // initialize on server startup.  No lag on first page load.
+	}
 
 }
