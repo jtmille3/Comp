88730b06c01b7e93eb150b2e21079bf0b9a4fbf1
Jeff Miller <jeffmiller@jeffmiller.name>
2013-04-01 20:39:52 -0400

    move team collation to another script

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,24 +1,13 @@
 /*global define */
 define(function(require) {
-    'use strict';
+	'use strict';
 
-    var compTemplate = window.comp['web/templates/comp.html'];
+	var compTemplate = window.comp['web/templates/comp.html'];
+	var teamHelper = require('./team');
+	$.getJSON('http://localhost:8080/comp/service/seasons', function(seasons) {
+		teamHelper.setTeams(seasons);
+		$('#seasons').append(compTemplate(seasons));
+	});
 
-    $.getJSON('http://localhost:8080/comp/service/seasons', function(result) {
-		for(var i = 0; i < result.length; i++)
-		{
-			var season = result[i];
-			var teamDict = {};
-			for(var j = 0;j < season.teams.length; j++)
-			{
-				var team = season.teams[j];
-				teamDict[team.id] = team;
-			}
-			season.teams = teamDict;
-		}
-    	console.log(result);
-        $('#seasons').append(compTemplate(result));
-    });
-
-    return 'Initialized';
+	return 'Initialized';
 });
