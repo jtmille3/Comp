56fa621e4575ebae685d3967b9951960f05d3410
Jeff Miller <jeffmiller@jeffmiller.name>
2013-04-06 23:09:19 -0400

    view team schedule

--- web/scripts/controller/competitive.js
+++ web/scripts/controller/competitive.js
@@ -3,6 +3,7 @@ define(function(require) {
 	'use strict';
 	return {
 		render: function(competitive) {
+			var self = this;
 			this.competitive = competitive;
 
 			/*
@@ -18,18 +19,36 @@ define(function(require) {
 			for(var i = 0; i < competitive.seasons.length; i++) {
 				var season = competitive.seasons[i];
 				$('#' + season.id + '-standings-table').tablesorter( {sortList: [[0,0]]} );
-				$('#' + season.id + '-playoff-table').tablesorter( {sortList: [[0,0]]} );
-				$('#' + season.id + '-schedule-table').tablesorter( {sortList: [[2,0]]} ); 
+				$('#' + season.id + '-playoff-schedule-table').tablesorter( {sortList: [[2,0]]} );
+				$('#' + season.id + '-league-schedule-table').tablesorter( {sortList: [[2,0]]} ); 
 				$('#' + season.id + '-player-statistics-table').tablesorter( {sortList: [[3,1]]} );
 				$('#' + season.id + '-goalie-statistics-table').tablesorter( {sortList: [[2,1]]} );
 
-				$('#' + season.id + '-standings-table tr').click(function(row) {
-					var $row = $(row.currentTarget);
-					var teamId = $row.attr('id');
-					$row.addClass('selected').siblings().removeClass('selected');
-					console.log('clicked ' + teamId);
-				});
+				this.attachStandingsClickHandler(season);
 			}
+		},
+		attachStandingsClickHandler: function(season) {
+			var self = this;
+			$('#' + season.id + '-standings-table tr').click(function(row) {
+				var $row = $(row.currentTarget);
+				var teamId = parseInt($row.attr('id'), 10);
+				$row.addClass('selected').siblings().removeClass('selected');
+				self.selectedTeam(season, teamId);
+			});
+		},
+		selectedTeam: function(season, teamId) {
+			console.log('clicked ' + teamId);
+			var teamSchedule = [];
+			for(var i = 0; i < season.leagueSchedule.length; i++) {
+				var schedule = season.leagueSchedule[i];
+				if(schedule.homeId === teamId || schedule.awayId === teamId) {
+					teamSchedule.push(schedule);
+				}
+			}
+
+			var teamScheduleTemplate = window.comp['web/templates/team_schedule.html'];
+			var template = teamScheduleTemplate(teamSchedule);
+			$('#' + season.id + '-team-schedule').replaceWith(template);
 		}
 	};
 });
