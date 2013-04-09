e21c073b96cdf41b007fd3c149190727b517136b
Jeff Miller <jeffmiller@jeffmiller.name>
2013-04-08 20:16:06 -0400

    drop shadow

--- src/com/sas/comp/resource/CompetitiveResource.java
+++ src/com/sas/comp/resource/CompetitiveResource.java
@@ -3,12 +3,13 @@ package com.sas.comp.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.sas.comp.listener.Cache;
 import com.sas.comp.models.Competitive;
 
 @Path("competitive")
-@Produces("application/json")
+@Produces(MediaType.APPLICATION_JSON)
 public class CompetitiveResource {
 
 	@GET
