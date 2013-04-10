734f84ec8cb20a2b552a64a00f2dc89b95cdf3f4
Jeff Miller <jeffmiller@jeffmiller.name>
2013-04-10 08:14:12 -0400

    way to query schedules by date using date selection

---
+++ src/com/sas/comp/resource/ScheduleResource.java
@@ -0,0 +1,27 @@
+package com.sas.comp.resource;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.sas.comp.models.Schedule;
+import com.sas.comp.service.ScheduleService;
+
+@Path("schedules")
+@Produces(MediaType.APPLICATION_JSON)
+public class ScheduleResource {
+
+	private final ScheduleService scheduleService = new ScheduleService();
+	private final SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy hh:mm");
+
+	@GET
+	public List<Schedule> getSchedules(@QueryParam("date") final String date) throws ParseException {
+		return scheduleService.getSchedules(sdf.parse(date));
+	}
+}
--- src/com/sas/comp/service/ScheduleService.java
+++ src/com/sas/comp/service/ScheduleService.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.sas.comp.models.Schedule;
@@ -27,6 +28,47 @@ public class ScheduleService {
 			final PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM schedule WHERE playoff = ? AND season_id = ?");
 			pstmt.setBoolean(1, playoff);
 			pstmt.setInt(2, seasonId);
+
+			final ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				final Schedule schedule = new Schedule();
+				schedule.setHome(rs.getString("home"));
+				schedule.setHomeId(rs.getInt("home_id"));
+				schedule.setAway(rs.getString("away"));
+				schedule.setAwayId(rs.getInt("away_id"));
+				schedule.setDate(rs.getTimestamp("date"));
+				schedule.setGameId(rs.getInt("game_id"));
+
+				schedule.setHomeScore(rs.getInt("home_score"));
+				if (rs.wasNull()) {
+					schedule.setHomeScore(null);
+				}
+
+				schedule.setAwayScore(rs.getInt("away_score"));
+				if (rs.wasNull()) {
+					schedule.setAwayScore(null);
+				}
+
+				schedules.add(schedule);
+			}
+
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+
+		return schedules;
+	}
+
+	public List<Schedule> getSchedules(final Date date) {
+		final List<Schedule> schedules = new ArrayList<Schedule>();
+
+		try {
+			final Connection conn = Database.getConnection();
+			final PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM schedule WHERE date = ?");
+			pstmt.setTimestamp(1, new java.sql.Timestamp(date.getTime()));
 
 			final ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
