d4ee20064a85ea4c03d9fef949e26529dc51d98d
Philippe Sabourin <philippe.sabourin@gmail.com>
2013-04-24 14:51:31 -0400

    Switching servers.

--- src/com/sas/comp/mysql/Database.java
+++ src/com/sas/comp/mysql/Database.java
@@ -7,6 +7,6 @@ public class Database {
 
 	public static Connection getConnection() throws Exception {
 		Class.forName("com.mysql.jdbc.Driver");
-		return DriverManager.getConnection("jdbc:mysql://d18891.na.sas.com:3306/soccer", "soccer", "i<3soccer");
+		return DriverManager.getConnection("jdbc:mysql://pip.na.sas.com:3306/soccer", "soccer", "i<3soccer");
 	}
 }
