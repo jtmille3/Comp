e52073fb2cb3e3409785d17f80a21cdd8b3a0171
Jeff Miller <jeffmiller@jeffmiller.name>
2014-03-17 23:05:04 -0400

    fixed new season standings

--- src/com/sas/comp/service/StandingService.java
+++ src/com/sas/comp/service/StandingService.java
@@ -1,51 +1,96 @@
 package com.sas.comp.service;
 
+import com.sas.comp.models.Standing;
+import com.sas.comp.mysql.Database;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sas.comp.models.Standing;
-import com.sas.comp.mysql.Database;
-
 public class StandingService {
 
-  public List<Standing> getStandings(final Integer seasonId) {
-    final List<Standing> standings = new ArrayList<Standing>();
-
-    try {
-      final Connection conn = Database.getConnection();
-      final PreparedStatement pstmt = conn
-          .prepareStatement("SELECT * FROM standings WHERE season_id = ? order by points desc, goal_differential desc, goals_for desc");
-      pstmt.setInt(1, seasonId);
-
-      final ResultSet rs = pstmt.executeQuery();
-      int rank = 1;
-      while (rs.next()) {
-        final Standing standing = new Standing();
-        standing.setRank(rank++);
-        standing.setTeam(rs.getString("team"));
-        standing.setPoints(rs.getInt("points"));
-        standing.setWins(rs.getInt("wins"));
-        standing.setLosses(rs.getInt("losses"));
-        standing.setTies(rs.getInt("ties"));
-        standing.setGoalsFor(rs.getInt("goals_for"));
-        standing.setGoalsAgainst(rs.getInt("goals_against"));
-        standing.setGoalDifferential(rs.getInt("goal_differential"));
-        standing.setShutouts(rs.getInt("shutouts"));
-        standing.setTeamId(rs.getInt("team_id"));
-        standings.add(standing);
-      }
-
-      rs.close();
-      pstmt.close();
-      conn.close();
-    } catch (final Exception e) {
-      e.printStackTrace();
+    public List<Standing> getStandings(final Integer seasonId) {
+        final List<Standing> standings = getSeasonStandings(seasonId);
+        if(standings.size() == 0) {
+            return getEmptyStandings(seasonId);
+        } else {
+            return standings;
+        }
     }
 
-    return standings;
-  }
+    private List<Standing> getEmptyStandings(final Integer seasonId) {
+        final List<Standing> standings = new ArrayList<Standing>();
+
+        try {
+            final Connection conn = Database.getConnection();
+            final PreparedStatement pstmt = conn
+                    .prepareStatement("SELECT id, name FROM teams WHERE season_id = ? ORDER BY name");
+            pstmt.setInt(1, seasonId);
+
+            final ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                final Standing standing = new Standing();
+                standing.setTeam(rs.getString("name"));
+                standing.setTeamId(rs.getInt("id"));
+                standing.setRank(0);
+                standing.setPoints(0);
+                standing.setWins(0);
+                standing.setLosses(0);
+                standing.setTies(0);
+                standing.setGoalsFor(0);
+                standing.setGoalsAgainst(0);
+                standing.setGoalDifferential(0);
+                standing.setShutouts(0);
+                standings.add(standing);
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        return standings;
+    }
+
+    private List<Standing> getSeasonStandings(final Integer seasonId) {
+        final List<Standing> standings = new ArrayList<Standing>();
+
+        try {
+            final Connection conn = Database.getConnection();
+            final PreparedStatement pstmt = conn
+                    .prepareStatement("SELECT * FROM standings WHERE season_id = ? order by points desc, goal_differential desc, goals_for desc");
+            pstmt.setInt(1, seasonId);
+
+            final ResultSet rs = pstmt.executeQuery();
+            int rank = 1;
+            while (rs.next()) {
+                final Standing standing = new Standing();
+                standing.setRank(rank++);
+                standing.setTeam(rs.getString("team"));
+                standing.setPoints(rs.getInt("points"));
+                standing.setWins(rs.getInt("wins"));
+                standing.setLosses(rs.getInt("losses"));
+                standing.setTies(rs.getInt("ties"));
+                standing.setGoalsFor(rs.getInt("goals_for"));
+                standing.setGoalsAgainst(rs.getInt("goals_against"));
+                standing.setGoalDifferential(rs.getInt("goal_differential"));
+                standing.setShutouts(rs.getInt("shutouts"));
+                standing.setTeamId(rs.getInt("team_id"));
+                standings.add(standing);
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        return standings;
+    }
 
 }
