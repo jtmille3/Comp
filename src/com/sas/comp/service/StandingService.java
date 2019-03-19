3441217491895ed37b6b83edc202d08290a10250
JackOfManyTrades <jack.s.imbriani@gmail.com>
2019-03-19 13:34:01 -0400

    Fix for standings not showing up for teams who haven't played a game but where other teams have.

--- src/com/sas/comp/service/StandingService.java
+++ src/com/sas/comp/service/StandingService.java
@@ -18,6 +18,10 @@ public class StandingService {
     
     private void populateAllStandings() {
 
+        // need empty standings for all teams or standings display will be empty when some teams have games played but others do not.
+        // the empty standings will be overwritten below for teams who have had games played
+        this.setAllEmptyStandings();
+
         Database.doVoidTransaction("SELECT * FROM standings order by season_id desc ,points desc, goal_differential desc, goals_for desc", (pstmt) -> {
             final ResultSet rs = pstmt.executeQuery();
             int rank = 0;
@@ -25,15 +29,16 @@ public class StandingService {
             while (rs.next()) {
                 Integer seasonId = rs.getInt("season_id");
                 List<Standing>standings = allStandings.get(seasonId);
-                if( standings == null ) standings = new ArrayList<>();
                 if( seasonId != prevSeasonId ) {
                     rank = 1;
                 } else {
                     rank++;
                 }
-                Standing standing = new Standing();
+                String team = rs.getString("team");
+                // find the empty standing for team and overwrite its values with non-empty data
+                Standing standing = standings.stream().filter(s -> s.getTeam().equals(team)).findFirst().get();
                 standing.setRank(rank);
-                standing.setTeam(rs.getString("team"));
+                standing.setTeam(team);
                 standing.setPoints(rs.getInt("points"));
                 standing.setWins(rs.getInt("wins"));
                 standing.setLosses(rs.getInt("losses"));
@@ -45,31 +50,24 @@ public class StandingService {
                 standing.setTeamId(rs.getInt("team_id"));
                 standing.setLeagueWinner(rs.getInt("leagueWinner"));
                 standing.setPlayoffWinner(rs.getInt("playoffWinner"));
-                standings.add(standing);
-                allStandings.put(seasonId, standings);
                 prevSeasonId = seasonId;
             }
         });
         
     }
-
+    
     public List<Standing> getStandings(final Integer seasonId) {
-        final List<Standing> standings = getSeasonStandings(seasonId);
-        if(standings == null || standings.size() == 0) {
-            return getEmptyStandings(seasonId);
-        } else {
-            return standings;
-        }
+        final List<Standing> standings = allStandings.get(seasonId);
+        return standings;
     }
 
-    private List<Standing> getEmptyStandings(final Integer seasonId) {
-        final List<Standing> standings = new ArrayList<>();
-
-        Database.doVoidTransaction("SELECT id, name FROM teams WHERE season_id = ? ORDER BY name", (pstmt) -> {
-            pstmt.setInt(1, seasonId);
-
+    private void setAllEmptyStandings() {
+        Database.doVoidTransaction("SELECT id, name, season_id FROM teams ORDER BY season_id, name", (pstmt) -> {
             final ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
+                Integer seasonId = rs.getInt("season_id");
+                List<Standing>standings = allStandings.get(seasonId);
+                if( standings == null ) standings = new ArrayList<>();
                 final Standing standing = new Standing();
                 standing.setTeam(rs.getString("name"));
                 standing.setTeamId(rs.getInt("id"));
@@ -85,15 +83,9 @@ public class StandingService {
                 standing.setLeagueWinner(0);
                 standing.setPlayoffWinner(0);
                 standings.add(standing);
+                allStandings.put(seasonId, standings);
             }
         });
-
-        return standings;
-    }
-
-    private List<Standing> getSeasonStandings(final Integer seasonId) {
-        final List<Standing> standings = allStandings.get(seasonId);
-        return standings;
     }
 
 }
