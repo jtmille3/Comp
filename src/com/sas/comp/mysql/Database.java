170d49bdccf6a6143f826430abed52d3011b88dd
Jack Imbriani <Jack.Imbriani@sas.com>
2019-03-20 08:14:12 -0400

    DB driver / config changes

--- src/com/sas/comp/mysql/Database.java
+++ src/com/sas/comp/mysql/Database.java
@@ -8,7 +8,7 @@ public class Database {
 
 	private static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
-		//return DriverManager.getConnection("jdbc:mysql://pip.na.sas.com:3306/soccer", "soccer", "i<3soccer");
+		//return DriverManager.getConnection("jdbc:mysql://pip.na.sas.com:3306/soccer?useSSL=false", "soccer", "i<3soccer");
         return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/soccer?useSSL=false", "soccer", "i<3soccer");
 	}
 
